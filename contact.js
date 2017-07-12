b7c8fa302314fb9174bb2d89f59c343dbfe7fad1
Tony Gunn <khiidt95@gmail.com>
2017-07-12 13:07:09 -0500

    update to nav/plus content

---
+++ contact.js
@@ -0,0 +1,13 @@
+function submitContactForm (){
+     alert("Thanks For contacting us!")
+
+    var x = document.div["contactForm"]["email"].value;
+     if (x == "")
+        {
+        alert("Email must be filled");
+        }
+    else
+        {
+        alert("Thank you!")
+        }
+}
